2337b547088974eaacbee36564d5147f76e9c8b5
daniyal khan <daniyal.khan@afiniti.com>
2021-09-15 12:19:00 +0500

    prop drilling, removed user and login and instead get it from our auth context

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,8 @@
 // 🐨 create and export a React context variable for the AuthContext
 // 💰 using React.createContext
+
+const {createContext} = require('react')
+
+const AuthContext = createContext()
+
+export {AuthContext}
