f69f060c1079aeede4a459c7112cd8121ce7c0b7
daniyal khan <daniyal.khan@afiniti.com>
2021-09-15 11:34:50 +0500

    react query for cache and data fetch

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,7 +3,25 @@ import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './app'
+import {ReactQueryConfigProvider} from 'react-query'
+
+const confiqQuries = {
+  queries: {
+    retry(failureCount, error) {
+      if (error === '404') return false
+      else if (failureCount < 2) return true
+      else return false
+    },
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+  },
+}
 
 loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(
+    <ReactQueryConfigProvider config={confiqQuries}>
+      <App />
+    </ReactQueryConfigProvider>,
+    document.getElementById('root'),
+  )
 })
